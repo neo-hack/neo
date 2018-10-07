a69f8348464a1b2b0d80973e595ac9577d88fb09
JiangWeixian <saber_wei@163.com>
2018-10-07 18:56:39 +0800

    add linter for ts and styl

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,10 @@ import { render } from 'react-dom'
 
 const $ROOT = document.querySelector('#app')
 
-var arr = ['a', 'b', 'c']
-Array.from('abc')
-var array1 = [1, 2, 3];
-console.log(array1.includes(2));
-
-const renderApp = (App) => {
+const renderApp = (Component) => {
   console.log('render')
   render(
-    <App />,
+    <Component />,
     $ROOT
   )
 }
