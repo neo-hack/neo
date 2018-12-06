7f9dd9b101881e89e38ad51af559973a404b064d
JiangWeixian <saber_wei@163.com>
2018-12-06 18:27:53 +0800

    complete history listen

--- src/pages/Home/In/index.tsx
+++ src/pages/Home/In/index.tsx
@@ -1,10 +1,7 @@
 import * as React from 'react'
-import { addHistoryListener } from '@/watcher/history'
 
 class In extends React.Component {
   render () {
-    addHistoryListener({ pathname: '/home/in', callback: () => console.log('in'), id: 'in' })
-    addHistoryListener({ pathname: '/home/in', callback: () => console.log('in2'), id: 'in2' })
     return (
       <div>
         this is in page
--- src/watcher/history/index.ts
+++ src/watcher/history/index.ts
@@ -1,7 +1,5 @@
 import createHashHistory from 'history/createHashHistory'
-import { parse } from 'query-string'
-
-const isArray = require('lodash/isArray')
+const isEmpty = require('lodash/isEmpty')
 
 const $GLOBAL = 'global'
 let currentPathname = $GLOBAL
@@ -20,13 +18,19 @@ interface ListenersSchema {
     },
   },
 }
+
 export const listeners: ListenersSchema = {}
+let listenersIds = []
 
 // Method
 interface AddListenerSchema {
   pathname: string
   callback: (val: any) => any
   id: string,
+}
+
+const updateListenerIds = () => {
+  listenersIds = Object.keys(listeners)
 }
 
 export const addHistoryListener = ({ pathname, callback, id }: AddListenerSchema) => {
@@ -49,15 +53,60 @@ export const addHistoryListener = ({ pathname, callback, id }: AddListenerSchema
     }
     listeners[pathname].ids = Object.keys(callbacks)
   }
+  updateListenerIds()
+}
+  
+
+interface RemoveListenerSchema {
+  pathname: string,
+  id: string
+}
+
+export const removeHistoryListener = ({ pathname, id }: RemoveListenerSchema) => {
+  if (!listeners[pathname]) return null
+  const { callbacks } = listeners[pathname]
+  if (!callbacks[id]) {
+    console.info('ID not exit')
+    return null
+  }
+  delete callbacks[id]
+  listeners[pathname].ids = Object.keys(callbacks)
+  updateListenerIds()
+}
+
+// Emit Global Listeners
+const emitPathListeners = (location, pathname) => {
+  if (!listeners[pathname]) return null
+  if (isEmpty(listeners[pathname])) return null
+  const { ids, callbacks } = listeners[pathname]
+  if (!ids || isEmpty(ids)) return null
+  ids.forEach(item => {
+    const { callback } = callbacks[item]
+    if (!callback) return null
+    callback(location)
+  })
+}
+
+const emitListeners = (location) => {
+  const { pathname } = location
+  const validPathname = listenersIds.filter(item => {
+    if (pathname.startsWith(item) || item === pathname) return true
+    if (!item) return false
+    return false
+  })
+  // Global
+  emitPathListeners(location, $GLOBAL)
+  // Current
+  validPathname.forEach(item => {
+    emitPathListeners(location, item)
+  })
 }
 
 // Entry
 const history = createHashHistory()
 
 history.listen((val) => {
-  const { pathname } = val
-  currentPathname = pathname
-  console.log(listeners)
+  emitListeners(val)
 })
 
 export default history
