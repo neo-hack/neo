46ec370a5f770c0bb9300594a957872e806ea41c
JiangWeixian <saber_wei@163.com>
2019-05-11 11:46:00 +0800

    fix: rm history watcher

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,31 +1,27 @@
-import * as React from 'react'
-import ExampleCount from '@/components/Examples/Count'
-import SubRoutes from '@/routes/SubRoutes'
-import history, { addHistoryListener } from '@/watcher/history'
-import { Switch } from 'react-router-dom'
+import * as React from "react";
+import ExampleCount from "@/components/Examples/Count";
+import SubRoutes from "@/routes/SubRoutes";
+import { Switch } from "react-router-dom";
 
 interface PageProps {
-  routes?: any
+  routes?: any;
 }
 
-class Home extends React.Component <PageProps> {
-  render () {
-    const { routes } = this.props
-    addHistoryListener({ pathname: '/home', callback: () => console.log('home'), id: 'home' })
+class Home extends React.Component<PageProps> {
+  render() {
+    const { routes } = this.props;
     return (
       <div>
         this is new home page
         <ExampleCount />
         <Switch>
-          {
-            routes.map((route, i) => {
-              return SubRoutes(route, i)
-            })
-          }
+          {routes.map((route, i) => {
+            return SubRoutes(route, i);
+          })}
         </Switch>
       </div>
-    )
+    );
   }
 }
 
-export default Home
+export default Home;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,21 +1,19 @@
-import * as React from 'react'
-import SubRoutes from './SubRoutes'
-import history from '@/watcher/history'
-import { HashRouter, Switch, Router } from 'react-router-dom'
-import { routesConfig } from './config'
+import * as React from "react";
+import SubRoutes from "./SubRoutes";
+import { createHashHistory } from "history";
+import { Switch, Router } from "react-router-dom";
+import { routesConfig } from "./config";
 
 const RouterViewer = () => {
   return (
-    <Router history={ history }>
+    <Router history={createHashHistory()}>
       <Switch>
-        {
-          routesConfig.map((route, i) => {
-            return SubRoutes(route, i)
-          })
-        }
+        {routesConfig.map((route, i) => {
+          return SubRoutes(route, i);
+        })}
       </Switch>
     </Router>
-  )
-}
+  );
+};
 
-export default RouterViewer
+export default RouterViewer;
