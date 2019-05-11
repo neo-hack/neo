3fd445e9c6f076c35449b8e0a0308e9ce54a8160
JiangWeixian <saber_wei@163.com>
2019-05-11 12:38:31 +0800

    init types css-modules

--- build/webpack.dev.config.js
+++ build/webpack.dev.config.js
@@ -1,20 +1,20 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
-const MergeWebpack = require('webpack-merge')
-const webpack = require('webpack')
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const FriendlyErrorsPlugin = require("friendly-errors-webpack-plugin");
+const MergeWebpack = require("webpack-merge");
+const webpack = require("webpack");
 
-const configs = require('./config').dev
-const commonWebpackConfig = require('./webpack.common.config')
+const configs = require("./config").dev;
+const commonWebpackConfig = require("./webpack.common.config");
 
-process.env.NODE_ENV = configs.mode
+process.env.NODE_ENV = configs.mode;
 
 const devWebpackConfig = {
-  devtool: 'cheap-module-eval-source-map',
+  devtool: "cheap-module-eval-source-map",
   mode: configs.mode,
   output: {
     path: configs.distPath,
-    filename: '[name].js',
-    publicPath: '/',
+    filename: "[name].js",
+    publicPath: "/"
   },
   devServer: {
     port: configs.port,
@@ -22,54 +22,55 @@ const devWebpackConfig = {
     open: true,
     overlay: true,
     compress: true,
-    clientLogLevel: 'none',
-    quiet: true,
+    clientLogLevel: "none",
+    quiet: true
   },
   module: {
     rules: [
       {
         test: /\.css$/,
         use: [
-          { loader: 'style-loader', options: { sourceMap: true } },
-          { loader: "css-loader", options: { sourceMap: true } },
+          { loader: "style-loader", options: { sourceMap: true } },
+          { loader: "css-loader", options: { sourceMap: true } }
         ]
       },
       {
         test: /(\.styl$|\.stylus$)/,
         use: [
-          { loader: 'style-loader', options: { sourceMap: true } },
+          { loader: "style-loader", options: { sourceMap: true } },
           {
-            loader: "css-loader",
+            loader: "typings-for-css-modules-loader",
             options: {
               sourceMap: true,
               modules: true,
+              namedExport: true,
               localIdentName: "[name]_[local]___[hash:base64:5]"
+              // silent: true
             }
           },
-          { loader: 'stylus-loader', options: { sourceMap: true } }
+          { loader: "stylus-loader", options: { sourceMap: true } }
         ]
       }
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: 'index.html',
-      inject: true,
+      filename: "index.html",
+      template: "index.html",
+      inject: true
     }),
     new webpack.NamedModulesPlugin(),
     new FriendlyErrorsPlugin({
       compilationSuccessInfo: {
-        messages: ['Running here http://localhost:' + configs.port],
-        notes: ['Happy coding']
+        messages: ["Running here http://localhost:" + configs.port],
+        notes: ["Happy coding"]
       },
-      onErrors: function (severity, errors) {
+      onErrors: function(severity, errors) {
         // You can listen to errors transformed and prioritized by the plugin
         // severity can be 'error' or 'warning'
-      },
+      }
     })
   ]
-}
+};
 
-console.log(MergeWebpack(commonWebpackConfig, devWebpackConfig))
-module.exports = MergeWebpack(commonWebpackConfig, devWebpackConfig)
+module.exports = MergeWebpack(commonWebpackConfig, devWebpackConfig);
