31eef69bc90cd714b342a7073279af3c3eb430fe
JW <jiangweixian1994@gmail.com>
2019-08-04 14:11:25 +0800

    Config (#18)
    * chore: update tsconfig
    
    * chore: changelog
    
    * feat: sync scripts
    
    * fix: sync files
    
    * chore: prettier in tslint
    
    * chore: change log
    
    * chore: add sync script in package.json

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,12 @@
+## 20190804
+
+* sync config files
+
+## 20190801
+
+* publish template as npm
+* publish cli npm
+
 ## 20190512
 
 * use `lerna` as managerment
---
+++ scripts/sync.js
@@ -0,0 +1,38 @@
+const fs = require('fs-extra')
+const path = require('path')
+
+const files = [
+  '.prettierignore',
+  '.prettierrc',
+  'tslint.json',
+  '.gitignore'
+]
+
+const excludes = [
+  'core'
+]
+
+/**
+ * sync confiles to each target folder
+ * @param {string} package target folder name
+ * @param {string[]} files sync files 
+ */
+const syncConfigs = (package, files) => {
+  files.forEach((f) => {
+    fs.copySync(
+      path.resolve(__dirname, `../${f}`),
+      path.resolve(__dirname, `../packages/${package}/${f}`)
+    )
+  })
+}
+
+fs
+  .readdir(path.resolve(__dirname, '../packages'))
+  .then(packages => {
+    const _packages = packages.filter(f => {
+      return excludes.findIndex(e => e === f) < 0
+    })
+    _packages.forEach(p => {
+      syncConfigs(p, files)
+    })
+  })
