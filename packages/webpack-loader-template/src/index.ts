e4044dc805426a6966d4f93842f0c443630c085a
JiangWeixian <saber_wei@163.com>
2019-09-13 22:57:54 +0800

    chore: update config

--- packages/webpack-loader-template/src/index.ts
+++ packages/webpack-loader-template/src/index.ts
@@ -4,7 +4,7 @@ import { getOptions } from './utils/options'
 
 function loader(this: Webpack.loader.LoaderContext, source: string) {
   const options = getOptions(this)
-  if (!options.include || !options.exclude) {
+  if (options.include && options.exclude) {
     return source
   }
   return source
