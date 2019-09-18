ac5bcf9396f61c4a81657666b3f15994b320c8ec
JiangWeixian <saber_wei@163.com>
2019-09-18 15:19:44 +0800

    fix: build

--- packages/ts-lib-template/src/index.ts
+++ packages/ts-lib-template/src/index.ts
@@ -1,13 +1,8 @@
-import Webpack from 'webpack'
-
 import { getOptions } from './utils/options'
 
-function loader(this: Webpack.loader.LoaderContext, source: string) {
-  const options = getOptions(this)
-  if (options.include && options.exclude) {
-    return source
-  }
-  return source
+function loader() {
+  const options = getOptions([])
+  return options
 }
 
 export default loader
