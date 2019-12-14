23d1ab94d724cf63805bc4cff820527b78fac533
JiangWeixian <saber_wei@163.com>
2019-12-14 18:04:32 +0800

    chore: update

--- packages/react-template/src/App.tsx
+++ packages/react-template/src/App.tsx
@@ -2,15 +2,13 @@ import React from 'react'
 import RouterViewer from '@/routes'
 import './App.styl'
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="app">
-        this is react-simple webpack template
-        <RouterViewer />
-      </div>
-    )
-  }
+const App = () => {
+  return (
+    <div className="app">
+      this is react-simple webpack template
+      <RouterViewer />
+    </div>
+  )
 }
 
 let AppContainer = App
