9928bbbc32a3cb83de38e00a5a3888132ee60826
JiangWeixian <saber_wei@163.com>
2020-04-09 11:01:44 +0800

    ✨ (core): whoami

---
+++ packages/core/src/neo-whoami.ts
@@ -0,0 +1,6 @@
+import * as terminalImage from 'terminal-image'
+import * as path from 'path'
+
+;(async () => {
+  console.log(await terminalImage.file(path.resolve(__dirname, './assets/neo.jpg')))
+})()
