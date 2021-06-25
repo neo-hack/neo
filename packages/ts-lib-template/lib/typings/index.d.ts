ffbea4216510305fb5450400215c83b73f7d1d43
JiangWeixian <jiangweixian1994@gmail.com>
2021-06-25 13:57:09 +0800

    :recycle:  tslint2eslint

---
+++ packages/ts-lib-template/lib/typings/index.d.ts
@@ -0,0 +1,1 @@
+export declare type Name = '@aiou/ts-lib-template'
--- scripts/sync.js
+++ scripts/sync.js
@@ -1,7 +1,7 @@
 const fs = require('fs-extra')
 const path = require('path')
 
-const files = ['.prettierignore', '.prettierrc', 'tslint.json', '.gitignore', 'LICENSE']
+const files = ['.prettierignore', '.prettierrc', '.eslintrc.js', '.gitignore', 'LICENSE']
 
 const excludes = ['core']
 
@@ -11,7 +11,7 @@ const excludes = ['core']
  * @param {string[]} files sync files
  */
 const syncConfigs = (package, files) => {
-  files.forEach(f => {
+  files.forEach((f) => {
     fs.copySync(
       path.resolve(__dirname, `../${f}`),
       path.resolve(__dirname, `../packages/${package}/${f}`),
@@ -23,7 +23,7 @@ const syncConfigs = (package, files) => {
  * sync .github folder
  * @param {string} package package in packages folder
  */
-const syncGithub = package => {
+const syncGithub = (package) => {
   fs.copySync(
     path.resolve(__dirname, `../.github`),
     path.resolve(__dirname, `../packages/${package}/.github`),
@@ -35,7 +35,7 @@ const syncGithub = package => {
  * @param {string[]} files
  */
 const syncTemplate = (package, files = []) => {
-  files.forEach(f => {
+  files.forEach((f) => {
     fs.copySync(
       path.resolve(__dirname, `../${f}`),
       path.resolve(__dirname, `../packages/${package}/template/${f}.tpl`),
@@ -43,11 +43,11 @@ const syncTemplate = (package, files = []) => {
   })
 }
 
-fs.readdir(path.resolve(__dirname, '../packages')).then(packages => {
-  const _packages = packages.filter(f => {
-    return excludes.findIndex(e => e === f) < 0
+fs.readdir(path.resolve(__dirname, '../packages')).then((packages) => {
+  const _packages = packages.filter((f) => {
+    return excludes.findIndex((e) => e === f) < 0
   })
-  _packages.forEach(p => {
+  _packages.forEach((p) => {
     syncConfigs(p, files)
     syncGithub(p)
     syncTemplate(p, files)
