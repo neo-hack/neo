5de319c50f172b2ce20e92b6cd4d192415854542
jiangWeixian <jiangweixian1994@gmail.com>
2021-07-19 00:15:24 +0800

    :rotating_light:  fix

--- packages/nextjs-rematch2-template/pages/api/users/index.ts
+++ packages/nextjs-rematch2-template/pages/api/users/index.ts
@@ -1,11 +1,12 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { api } from '~/request'
 
-export default async (_: NextApiRequest, res: NextApiResponse) => {
+export default async(_: NextApiRequest, res: NextApiResponse) => {
   try {
     const items = await api.users.list()
     res.status(200).json(items)
-  } catch (err) {
+  }
+  catch (err) {
     res.status(500).json({ statusCode: 500, message: err.message })
   }
 }
