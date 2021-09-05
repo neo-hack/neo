8cd481c832bb2c1915c64165a1c5b88e02d1943f
jiangWeixian <jiangweixian1994@gmail.com>
2021-09-05 14:56:43 +0800

    :white_check_mark: (core) version

--- packages/core/test/v.test.ts
+++ packages/core/test/v.test.ts
@@ -1,3 +1,11 @@
+import execa from 'execa'
+import pkg from '../package.json'
+const cli = require.resolve('../lib/neo.js')
+
 describe('command version', () => {
-  it.todo('print correct version')
+  it('print correct version', async () => {
+    const { stdout } = await execa('node', [cli].concat(['-V']))
+    const version = stdout.split('\n').filter((line) => !!line)[0]
+    expect(version).toEqual(pkg.version)
+  })
 })
