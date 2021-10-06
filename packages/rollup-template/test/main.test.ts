a061f1c1eb1469e86f872892bfb71562298a6424
JW <jiangweixian1994@gmail.com>
2021-10-06 20:04:37 +0800

    :boom: (rollup) migrate to standalone template (#218)

--- packages/rollup-template/test/main.test.ts
+++
@@ -1,9 +0,0 @@
-import { welcome } from '../src'
-
-describe('index', () => {
-  test('demo part', () => {
-    console.log = jest.fn()
-    welcome()
-    expect(console.log).toHaveBeenCalledWith('hello world')
-  })
-})
