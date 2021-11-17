09cef5e17465c1d719429cd3221b11b80a9a54d0
JW <jiangweixian1994@gmail.com>
2021-11-17 20:17:14 +0800

    Create task list (#254)

--- packages/core/src/utils/index.ts
+++ packages/core/src/utils/index.ts
@@ -1,6 +1,9 @@
 import { fileURLToPath } from 'url'
 import path, { dirname } from 'path'
+import { findUp } from 'find-up'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export const r = (pathname = '.') => path.resolve(__dirname, '..', '..', pathname)
+
+export const isMonorepo = () => findUp('pnpm-workspace.yaml')
