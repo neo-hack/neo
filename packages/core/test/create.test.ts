58d5b7ae9bfd4f1bc597e034b04eaefbafc00683
JW <jiangweixian1994@gmail.com>
2021-11-17 20:17:14 +0800

    Create task list (#254)

--- packages/core/test/create.test.ts
+++ packages/core/test/create.test.ts
@@ -1,13 +1,28 @@
-describe('command create', () => {
-  it.todo('create bin-template')
-  it.todo('create react-template')
-  it.todo('create alfred-workflow-template')
-  it.todo('create chrome-extension-template')
-  it.todo('create dayjs-plugin-template')
-  it.todo('create eslint-config-template')
-  it.todo('create original-html-template')
-  it.todo('create react-components-lib-template')
-  it.todo('create rollup-template')
-  it.todo('create ts-lib-template')
-  it.todo('create webpack-loader-template')
+import execa from 'execa'
+import path from 'path'
+import fs from 'fs-extra'
+
+import { r } from '../src/utils'
+
+const fixture = (pathname) => r(`test/fixtures/create/${pathname}`)
+const ensureDir = (pathname) => {
+  const folder = fixture(pathname)
+  if (!fs.existsSync(folder)) {
+    fs.mkdirSync(folder)
+  }
+}
+const clearDir = (pathname) => {
+  fs.removeSync(fixture(pathname))
+}
+const cli = r('lib/neo.js')
+
+describe('command create download npm package', () => {
+  it('create project from npm', async () => {
+    ensureDir('neo-cli-app')
+    await execa.node(cli, ['create', 'bin-template', 'neo-cli-app'], {
+      cwd: path.resolve(fixture('neo-cli-app')),
+    })
+    expect(fs.existsSync(fixture('neo-cli-app/neo-cli-app/README.md'))).toBe(true)
+    clearDir('neo-cli-app')
+  }, 10000)
 })
