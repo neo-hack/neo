0a48ed9f3429368a2bf19bcdffd7303108df3f83
JW <jiangweixian1994@gmail.com>
2021-12-10 10:58:40 +0800

    3.1.6 (#265)

--- packages/core/src/create.ts
+++ packages/core/src/create.ts
@@ -1,94 +1,64 @@
-// refs: https://github.com/vuejs/vue-cli/blob/v2/bin/vue-init
-
 import path from 'path'
-import { exec } from 'child_process'
-import { existsSync } from 'fs'
-import ora from 'ora'
+import execa from 'execa'
 import inquirer from 'inquirer'
 import download from 'download'
 import globby from 'globby'
-import rimraf from 'rimraf'
 import fsExtra from 'fs-extra'
 import InquirerSearchList from 'inquirer-search-list'
+import Listr, { ListrTask } from 'listr'
+import { isMonorepo } from './utils'
 
 import logger from './utils/logger'
 import { templates, TEMPLATES, SCOPE } from './utils/constants'
-
-const rm = rimraf.sync
 
 process.on('exit', () => {
   console.log()
 })
 
-const spinner = ora('downloading template')
-
 /**
  * download template .neo folder
  */
-const downloadNPM = ({ template }: { template: string }): Promise<boolean> => {
-  return new Promise((resolve, reject) => {
-    exec(`npm v @${SCOPE}/${template} dist.tarball`, (err, stdout) => {
-      if (err) {
-        console.log()
-        logger.fatal(`Failed to download template ${template}: ${err.message.trim()}`)
-        return reject(err)
-      }
-      download(stdout, path.join(process.cwd(), '.neo'), {
-        extract: true,
-      }).then(() => {
-        return resolve(true)
-      })
-    })
+const downloadNPM = async ({ template }: { template: string }) => {
+  const { stdout } = await execa('npm', ['v', `@${SCOPE}/${template}`, 'dist.tarball'])
+  await download(stdout, path.join(process.cwd(), '.neo'), {
+    extract: true,
   })
 }
 
 /**
  * generate template files
  */
-const generate = ({ dest, template }: { dest: string; template: string }) => {
-  fsExtra
-    .copy(path.join(process.cwd(), '.neo/package'), path.join(process.cwd(), dest))
-    .then(() => {
-      // remove cache files
-      fsExtra.remove(path.join(process.cwd(), '.neo'))
-      return true
-    })
-    .then(() => {
-      // generate config files from dest.template folder
-      const tplPath = path.join(process.cwd(), dest, 'template')
-      const tpls = globby.sync('*.tpl', {
-        cwd: tplPath,
-        dot: true,
-      })
-      tpls.forEach((f) => {
-        fsExtra.copySync(
-          path.join(tplPath, f),
-          path.join(process.cwd(), dest, f.replace('.tpl', '')),
-        )
-      })
-      // remove template folder
-      fsExtra.removeSync(tplPath)
-    })
-    .then(() => {
-      spinner.stop()
-      logger.success(`🎉 ${template} Generated 🎉!`)
-    })
+const generate = async ({ dest }: { dest: string }) => {
+  await fsExtra.copy(path.join(process.cwd(), '.neo/package'), path.join(process.cwd(), dest))
+  await fsExtra.remove(path.join(process.cwd(), '.neo'))
+  // generate config files from dest.template folder
+  const tplPath = path.join(process.cwd(), dest, 'template')
+  const tpls = globby.sync('*.tpl', {
+    cwd: tplPath,
+    dot: true,
+  })
+  tpls.forEach((f) => {
+    fsExtra.copySync(path.join(tplPath, f), path.join(process.cwd(), dest, f.replace('.tpl', '')))
+  })
+  // remove template folder
+  fsExtra.removeSync(tplPath)
 }
 
 /**
- * copy downloaded npm package to <projName> folder
+ * @description del files after generate
  */
-const downloadAndGenerate = ({ template, dest }: { template: string; dest: string }) => {
-  if (existsSync(template)) rm(template)
-  spinner.start()
-  downloadNPM({ template })
-    .then(() => {
-      generate({ dest, template })
+const postgenerate = async ({ dest }: { dest: string }) => {
+  const common = ['CHANGELOG.md']
+  const mono = ['.eslintignore', '.eslintrc', '.changeset', '.github', '.husky']
+  if (await isMonorepo()) {
+    common.concat(mono).forEach((filename) => {
+      fsExtra.removeSync(path.join(process.cwd(), dest, filename))
     })
-    .catch((err) => {
-      spinner.stop()
-      logger.fatal(`Failed to download template ${template}: ${err.message.trim()}`)
-    })
+    return
+  }
+  common.forEach((filename) => {
+    fsExtra.removeSync(path.join(process.cwd(), dest, filename))
+  })
 }
 
 const validateTemplates = (template: string) => {
@@ -97,18 +67,42 @@ const validateTemplates = (template: string) => {
   return Object.keys(templates).findIndex((v: string) => v === template) > -1
 }
 
-const run = (template: string, project: string) => {
-  // validate template in list
-  if (!validateTemplates(template)) {
-    logger.fatal(`Failed locate ${template}`)
-    return
+const createTask = ({ template, project }: { template: string; project: string }) => {
+  const hooks = {
+    validate: {
+      title: 'Validate template',
+      task: () => {
+        if (!validateTemplates(template)) {
+          throw new Error(`Failed locate ${template}`)
+        }
+        if (!project) {
+          throw new Error(`<project-name> is required`)
+        }
+      },
+    },
+    download: {
+      title: 'Download template',
+      task: async () => {
+        // TODO: looks not need
+        fsExtra.removeSync(template)
+        await downloadNPM({ template })
+      },
+    },
+    generate: {
+      title: 'Generate project',
+      task: async () => {
+        return generate({ dest: project })
+      },
+    },
+    // postgenerate
+    postgenerate: {
+      title: 'Clean up',
+      task: async () => {
+        return postgenerate({ dest: project })
+      },
+    },
   }
-  // project name is required
-  if (!project) {
-    logger.fatal('<project-name> is required')
-    return
-  }
-  downloadAndGenerate({ template, dest: project })
+  return new Listr(Object.values(hooks) as ListrTask<any>[])
 }
 
 inquirer.registerPrompt('search-list', InquirerSearchList)
@@ -116,9 +110,12 @@ inquirer.registerPrompt('search-list', InquirerSearchList)
 /**
  * @description create project from template
  */
-export const create = (template: string, project: string) => {
+export const create = async (template: string, project: string) => {
   if (template && project) {
-    run(template, project)
+    const task = createTask({ template, project })
+    await task.run()
+    console.log()
+    logger.success(`🎉 ${template} generated, Happy hacking!`)
   } else {
     console.log()
     inquirer
@@ -145,8 +142,11 @@ export const create = (template: string, project: string) => {
           message: 'Please enter a project name',
         },
       ])
-      .then((answers) => {
-        run(answers.template, answers.project)
+      .then(async (answers) => {
+        const task = createTask({ template: answers.template, project: answers.project })
+        await task.run()
+        console.log()
+        logger.success(`🎉 ${template} Generated, Happy hacking!`)
       })
       .catch(logger.fatal)
   }
