25af4dd2763378e5cc5a3dea73dbe7686d5e9509
jiangWeixian <jiangweixian1994@gmail.com>
2021-12-11 21:22:06 +0800

    :white_check_mark:  use tempy as destDir

--- packages/core/test/create.test.ts
+++ packages/core/test/create.test.ts
@@ -2,32 +2,17 @@ import path from 'path'
 import fs from 'fs-extra'
 import tempy from 'tempy'
 
-import { r } from '../src/utils'
 import { execNeo } from './helpers'
 
 const storeDir = path.join(tempy.directory(), '.store')
-
-const fixture = (pathname: string) => r(`test/fixtures/create/${pathname}`)
-const ensureDir = (pathname: string) => {
-  const folder = fixture(pathname)
-  if (!fs.existsSync(folder)) {
-    fs.mkdirSync(folder)
-  }
-}
-const clearDir = (pathname: string) => {
-  fs.removeSync(fixture(pathname))
-}
+const destDir = tempy.directory()
 
 describe('command create download npm package', () => {
   it('create project from npm', async () => {
-    ensureDir('neo-cli-app')
-    const { stdout, stderr } = await execNeo(
-      ['create', '@aiou/bin-template', 'neo-cli-app', '--store-dir', storeDir],
-      { cwd: path.resolve(fixture('neo-cli-app')) },
-    )
-    console.log(stdout, stderr)
-    expect(fs.existsSync(fixture('neo-cli-app/neo-cli-app/README.md'))).toBe(true)
-    clearDir('neo-cli-app')
+    await execNeo(['create', '@aiou/bin-template', 'neo-cli-app', '--store-dir', storeDir], {
+      cwd: destDir,
+    })
+    expect(fs.existsSync(path.join(destDir, './neo-cli-app/README.md'))).toBe(true)
   }, 10000)
 
   it.todo('create package will update lock file')
