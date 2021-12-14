013bdd826a6fd8c909bd4a944136eab1aec5f6ff
jiangWeixian <jiangweixian1994@gmail.com>
2021-12-14 23:26:30 +0800

    :bug:  cmd options

--- packages/core/test/v.test.ts
+++ packages/core/test/v.test.ts
@@ -3,7 +3,7 @@ import { execNeo } from './helpers'
 
 describe('command version', () => {
   it('print correct version', async () => {
-    const { stdout } = await execNeo(['-V'])
+    const { stdout } = await execNeo(['-v'])
     const version = stdout.split('\n').filter((line) => !!line)[0]
     expect(version).toEqual(pkg.version)
   })
