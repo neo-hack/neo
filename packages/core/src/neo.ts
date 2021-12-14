7c38868f1706f679854bb31ea3165c5a60d1ff25
jiangWeixian <jiangweixian1994@gmail.com>
2021-12-14 22:27:52 +0800

    :zap:  dynamic import

--- packages/core/src/neo.ts
+++ packages/core/src/neo.ts
@@ -4,11 +4,6 @@ import updateNotifier from 'update-notifier'
 import { readPackageUpSync } from 'read-pkg-up'
 
 import { r } from './utils'
-import { create } from './create'
-import { list } from './list'
-import { whoami } from './whoami'
-import { prepack } from './prepack'
-import { add } from './add'
 
 const pkg = readPackageUpSync({ cwd: r() })?.packageJson
 const notifier = updateNotifier({
@@ -18,36 +13,49 @@ notifier.notify()
 
 const cli = program.version(pkg?.version || '', '-v, --version')
 
+const commands = {
+  create: async () => await import('./create').then((res) => res.create),
+  list: async () => await import('./list').then((res) => res.list),
+  add: async () => await import('./add').then((res) => res.add),
+  prepack: async () => await import('./prepack').then((res) => res.prepack),
+  whoami: async () => await import('./whoami').then((res) => res.whoami),
+}
+
+const handler = (cmdName: string) => {
+  return async function (...args: any[]) {
+    const cmd = await commands[cmdName]
+    await cmd(...args)
+  }
+}
+
 cli
   .command('create [alias] [project]')
   .description('Generate a new project from a neo template')
   .alias('c')
   .option('--store-dir [storeDir]', 'Set store dir')
-  .action(create)
+  .action(handler('create'))
 
 cli
   .command('list')
   .description('List all templates')
   .alias('l')
   .option('--store-dir [storeDir]', 'Set store dir')
-  .action(list)
+  .action(handler('list'))
 
-cli.command('whoami').alias('me').description('Who is neo?').action(whoami)
+cli.command('whoami').alias('me').description('Who is neo?').action(handler('whoami'))
 
 cli
   .command('prepack')
   .alias('p')
   .description('Prepack neo ci, lint, husky, etc.. to your project')
   .option('-m, --module [modules...]', 'Prepack partial of ci, lint, husky, etc... to your project')
-  .action(prepack)
+  .action(handler('prepack'))
 
 cli
   .command('add [alias]')
   .description('Load template or preset into the `.neo-store`')
   .option('--store-dir [storeDir]', 'Set store dir')
   .option('--preset', 'If true, load `alias` as preset')
-  .action((alias, options = {}) => {
-    return add({ alias, options })
-  })
+  .action(handler('add'))
 
 program.parse(process.argv)
