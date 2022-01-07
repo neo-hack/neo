a40fd309a309df87e4e975875d2f166936710e96
jiangWeixian <jiangweixian1994@gmail.com>
2022-01-07 09:49:08 +0800

    :white_check_mark:  list configs and filtered by preset name

--- packages/core/test/cmd-list/list-config.test.ts
+++ packages/core/test/cmd-list/list-config.test.ts
@@ -17,17 +17,17 @@ describe('command list config', () => {
     expect(stdout).toMatchSnapshot()
   })
 
-  // it('list configs should list filtered by preset', async () => {
-  //   const { stdout } = await execNeo([
-  //     'list',
-  //     'configs',
-  //     '--store-dir',
-  //     storeDir,
-  //     '--no-interactive',
-  //     true,
-  //     '--preset',
-  //     'neo',
-  //   ])
-  //   expect(stdout).toMatchSnapshot()
-  // })
+  it('list configs should list filtered by preset', async () => {
+    const { stdout } = await execNeo([
+      'list',
+      'configs',
+      '--store-dir',
+      storeDir,
+      '--no-interactive',
+      true,
+      '--preset',
+      'neo',
+    ])
+    expect(stdout).toMatchSnapshot()
+  })
 })
