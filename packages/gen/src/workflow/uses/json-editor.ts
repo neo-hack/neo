39bc17c319a0d9f5e3dbc8a03fc08f005753df9b
jiangWeixian <jiangweixian1994@gmail.com>
2022-01-15 21:11:20 +0800

    :sparkles:  setup json with path

--- packages/gen/src/workflow/uses/json-editor.ts
+++ packages/gen/src/workflow/uses/json-editor.ts
@@ -1,13 +1,24 @@
 import JsonEditor from 'gulp-json-editor'
+import set from 'lodash.set'
+
 import { Action } from '../../interface'
 
 type JsonEditorOptions = {
   content: Record<string, any>
+  pairs: {
+    path: string
+    value: any
+  }[]
 }
 
 export const jsonEditor: Action<JsonEditorOptions> = (options) => {
   return JsonEditor(function (json: Record<string, any>) {
     const next = Object.assign(json, options.content)
+    if (options.pairs) {
+      options.pairs.forEach((pair) => {
+        set(next, pair.path, pair.value)
+      })
+    }
     return next
   })
 }
