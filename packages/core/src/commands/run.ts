8e814492f9f605e8a36cc77ce46dc5582332e278
jiangWeixian <jiangweixian1994@gmail.com>
2022-01-16 09:13:03 +0800

    :recycle:  prepack

---
+++ packages/core/src/commands/run.ts
@@ -0,0 +1,9 @@
+import { create } from '@aiou/gen'
+import { r } from '../utils'
+
+export const run = async (alias: string) => {
+  const workflow = await create(alias)
+  await workflow.start()
+}
+
+run(r('assets/prorepack.yaml'))
