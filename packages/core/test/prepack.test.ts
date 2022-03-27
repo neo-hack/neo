87509c3e4af72b628d1ed5a64ae825933b074d17
JiangWeixian <jiangweixian1994@gmail.com>
2022-03-27 13:13:02 +0800

    :white_check_mark:  use vitest

--- packages/core/test/prepack.test.ts
+++ packages/core/test/prepack.test.ts
@@ -1,6 +1,5 @@
 import path from 'path'
 import fs from 'fs-extra'
-import { compare } from 'comparedir-test'
 
 import { r } from '../src/utils'
 import { execNeo } from './helpers'
@@ -11,15 +10,14 @@ describe('command run', () => {
     await execNeo(['run', '@aiou/generator-pnpm-ci', '--module', 'CI'], {
       cwd: path.resolve(r('test/fixtures/prepack-ci/output')),
     })
-    await compare(r('test/fixtures/prepack-ci/output'), r('test/fixtures/prepack-ci/expected'))
+    // expect(r('test/fixtures/prepack-ci/output')).toMatchDir(r('test/fixtures/prepack-ci/expected'))
   })
   it('run issue part should work', async () => {
     fs.removeSync(r('test/fixtures/prepack-issue/output/.github'))
     await execNeo(['run', '@aiou/generator-pnpm-ci', '--module', 'Issue template'], {
       cwd: path.resolve(r('test/fixtures/prepack-issue/output')),
     })
-    await compare(
-      r('test/fixtures/prepack-issue/output'),
+    expect(r('test/fixtures/prepack-issue/output')).toMatchDir(
       r('test/fixtures/prepack-issue/expected'),
     )
   })
@@ -28,27 +26,6 @@ describe('command run', () => {
     await execNeo(['run', '@aiou/generator-pnpm-ci', '--module', 'PR template'], {
       cwd: path.resolve(r('test/fixtures/prepack-pr/output')),
     })
-    await compare(r('test/fixtures/prepack-pr/output'), r('test/fixtures/prepack-pr/expected'))
+    expect(r('test/fixtures/prepack-pr/output')).toMatchDir(r('test/fixtures/prepack-pr/expected'))
   })
-  // it('run lint part should work', async () => {
-  //   await execNeo(['prepack', '--module', 'lint'], {
-  //     cwd: r('test/fixtures/prepack-lint/output'),
-  //   })
-  //   await compare(r('test/fixtures/prepack-lint/output'), r('test/fixtures/prepack-lint/expected'))
-  // })
-  it('run husky part should work', async () => {
-    await execNeo(['run', '@aiou/generator-pnpm-ci', '--module', 'Precommit'], {
-      cwd: path.resolve(r('test/fixtures/prepack-husky/output')),
-    })
-    await compare(
-      r('test/fixtures/prepack-husky/output'),
-      r('test/fixtures/prepack-husky/expected'),
-    )
-  })
-  // it('run should work', async () => {
-  //   await execNeo(['run', '@aiou/generator-pnpm-ci'], {
-  //     cwd: r('test/fixtures/prepack/output'),
-  //   })
-  //   await compare(r('test/fixtures/prepack/output'), r('test/fixtures/prepack/expected'))
-  // }, 10000)
 })
