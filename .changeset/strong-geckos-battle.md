6d332d119879de51c4f82b759ce68a442df96037
JiangWeixian <jiangweixian1994@gmail.com>
2022-08-07 08:32:22 +0800

    :wrench: (mario) update build config

---
+++ .changeset/strong-geckos-battle.md
@@ -0,0 +1,5 @@
+---
+"@aiou/neo": minor
+---
+
+require node14
--- packages/core/rollup.config.js
+++ packages/core/rollup.config.js
@@ -3,9 +3,9 @@ import commonjs from '@rollup/plugin-commonjs'
 import json from '@rollup/plugin-json'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import { defineConfig } from 'rollup'
+import esbuild from 'rollup-plugin-esbuild'
 import size from 'rollup-plugin-size'
 import { terser } from 'rollup-plugin-terser'
-import ts from 'rollup-plugin-typescript2'
 
 export default defineConfig([
   // CommonJS (for Node) and ES module (for bundlers) build.
@@ -19,9 +19,9 @@ export default defineConfig([
     preserveEntrySignatures: 'strict',
     external: ['readable-stream'],
     plugins: [
-      ts({
-        check: false,
-        tsconfig: './tsconfig.build.json',
+      esbuild({
+        target: 'es2020',
+        minify: true,
       }),
       // TODO: external or include
       // fix: https://github.com/rollup/rollup/issues/1507
