7920437f316979d35d1fc5773f6e2f6c6db7e168
JW <jiangweixian1994@gmail.com>
2023-05-27 16:27:10 +0800

    :fire:  merge prepack.test

--- packages/core/test/cmd-run/run.test.ts
+++ packages/core/test/cmd-run/run.test.ts
@@ -1,3 +1,8 @@
+import path from 'node:path'
+
+import fs from 'fs-extra'
+
+import { r } from '../../src/utils'
 import {
   execNeo,
   mockLockFile,
@@ -10,10 +15,37 @@ beforeAll(async () => {
 
 describe('command run', () => {
   it('run mario with version', async () => {
-    const { stdout } = await execNeo(
-      ['run', '@aiou/generator-aiou@0.2.1', '--store-dir', storeDir],
-      { cwd: __dirname },
+    await execNeo(
+      ['run', '@aiou/generator-pnpm-ci@2.1.1', '--store-dir', storeDir],
+      { cwd: path.resolve(r('test/fixtures/prepack/output')) },
     )
-    expect(stdout).toMatchSnapshot()
+    // FIXME: there is bug in toMatchDir(always pass true)
+    expect(r('test/fixtures/prepack/output')).toMatchDir(
+      r('test/fixtures/prepack/expected'),
+    )
+  })
+
+  it('run ci part should work', async () => {
+    fs.removeSync(r('test/fixtures/prepack-ci/output/.github'))
+    await execNeo(['run', '@aiou/generator-pnpm-ci', '--module', 'CI'], {
+      cwd: path.resolve(r('test/fixtures/prepack-ci/output')),
+    })
+    // expect(r('test/fixtures/prepack-ci/output')).toMatchDir(r('test/fixtures/prepack-ci/expected'))
+  })
+  it('run issue part should work', async () => {
+    fs.removeSync(r('test/fixtures/prepack-issue/output/.github'))
+    await execNeo(['run', '@aiou/generator-pnpm-ci', '--module', 'Issue template'], {
+      cwd: path.resolve(r('test/fixtures/prepack-issue/output')),
+    })
+    expect(r('test/fixtures/prepack-issue/output')).toMatchDir(
+      r('test/fixtures/prepack-issue/expected'),
+    )
+  })
+  it('run pr part should work', async () => {
+    fs.removeSync(r('test/fixtures/prepack-pr/output/.github'))
+    await execNeo(['run', '@aiou/generator-pnpm-ci', '--module', 'PR template'], {
+      cwd: path.resolve(r('test/fixtures/prepack-pr/output')),
+    })
+    expect(r('test/fixtures/prepack-pr/output')).toMatchDir(r('test/fixtures/prepack-pr/expected'))
   })
 })
